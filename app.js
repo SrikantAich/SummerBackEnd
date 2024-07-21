77bea4e6cf773e3f929a329b55a34123c7aff51b
SrikantAich <srikant.12111982@lpu.in>
2024-07-21 20:46:14 +0530

    domain check cors

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-   origin: 'http://localhost:3000', // Allow requests from your React app's domain
+   origin: 'https://summerpep.srikant.tech', // Allow requests from your React app's domain
    methods: ['GET', 'POST', 'PUT', 'DELETE'],
    allowedHeaders: ['Content-Type', 'Authorization'],
 }));
