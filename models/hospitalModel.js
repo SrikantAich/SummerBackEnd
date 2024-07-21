46bdf83c57c0030e6bf073525ef124e720e2fbe1
SrikantAich <srikant.12111982@lpu.in>
2024-07-21 20:28:43 +0530

    mongoose model

--- models/hospitalModel.js
+++ models/hospitalModel.js
@@ -5,7 +5,11 @@ const hospitalSchema = new mongoose.Schema({
     city: { type: String, required: true },
     image: { type: String, required: true },
     speciality: { type: [String], required: true },
-    rating: { type: Number, required: true }
-});
+    rating: { type: Number, required: true },
+    description: { type: String, required: true }, // New field
+    extraImageUrl: { type: String, default: '' }, // New field
+    numberOfDoctors: { type: Number, required: true }, // New field
+    numberOfDepartments: { type: Number, required: true } // New field
+}, { timestamps: true }); // Added timestamps for createdAt and updatedAt
 
 module.exports = mongoose.model('Hospital', hospitalSchema);
